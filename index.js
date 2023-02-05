6ea1f949ee9597e565d81c6b82b4e58d5436da74
Jingyao Liu <ljyRoger@gmail.com>
2023-02-05 15:10:50 +0800

    添加读取原始market的方法

--- index.js
+++ index.js
@@ -1,110 +1,67 @@
 const puppeteer = require('puppeteer')
 
 async function getAllMatches(browserPage) {
-  const handleWebSocketFrameReceived = async (params, resolve) => {
+  const handleMatched = function (matches, result) {
+    for (const match of matches.filter(Boolean)) {
+      try {
+        const { id, fixture, markets, meta } = match
+        const { competitors, score, status, startTime, tournament } = fixture
 
+        const { value: mapIndex } = meta.find(spec => spec.name === "state_number") || {}
+        const { value: sideAway } = meta.find(spec => spec.name === "side_away") || {}
+        const { value: sideHome } = meta.find(spec => spec.name === "side_home") || {}
+        const { value: bo } = meta.find(spec => spec.name === "bo") || {}
+
+        const filtered_markets = markets.filter((item) => { return item.id == "7m1" || item.id == "7m2" })
+
+        const { name: tournamentName, id: tournamentId } = tournament
+        const { name: home, score: homeScore } = competitors.find(cmp => /home/i.test(cmp.homeAway))
+        const { name: away, score: awayScore } = competitors.find(cmp => /away/i.test(cmp.homeAway))
+
+        const { points: homeCurrentPoint } = homeScore.find(score => score.number === parseInt(mapIndex))
+        const { points: awayCurrentPoint } = awayScore.find(score => score.number === parseInt(mapIndex))
+
+        result[id] = {
+          id,
+          originalId: id.length > 36 ? id.slice(-36) : id,
+          score,
+          status,
+          startTime: +new Date(startTime),
+          home: {
+            name: home,
+            currentPoint: homeCurrentPoint,
+            side: sideHome
+          },
+          away: {
+            name: away,
+            currentPoint: awayCurrentPoint,
+            side: sideAway
+          },
+          markets: filtered_markets,
+          mapIndex,
+          tournamentName,
+          tournamentId,
+          bo
+        }
+      } catch (e) {
+        console.log(e)
+      }
+    }
+  }
+  const handleWebSocketFrameReceived = async (params, resolve) => {
+    const result = {}
     try {
       const data = JSON.parse(params.response.payloadData)
       // console.log("received data", data);
       if (data && data.payload && data.payload.data && data.payload.data.matches) {
-        const result = {}
         const { matches } = data.payload.data
-
-        for (const match of matches.filter(Boolean)) {
-          try {
-            const { id, fixture, markets, meta } = match
-            const { competitors, score, status, startTime, tournament } = fixture
-
-            const { value: mapIndex } = meta.find(spec => spec.name === "state_number") || {}
-            const { value: sideAway } = meta.find(spec => spec.name === "side_away") || {}
-            const { value: sideHome } = meta.find(spec => spec.name === "side_home") || {}
-            const { value: bo } = meta.find(spec => spec.name === "bo") || {}
-
-
-            const { name: tournamentName, id: tournamentId } = tournament
-            const { name: home, score: homeScore } = competitors.find(cmp => /home/i.test(cmp.homeAway))
-            const { name: away, score: awayScore } = competitors.find(cmp => /away/i.test(cmp.homeAway))
-
-            const { points: homeCurrentPoint } = homeScore.find(score => score.number === parseInt(mapIndex))
-            const { points: awayCurrentPoint } = awayScore.find(score => score.number === parseInt(mapIndex))
-
-            result[id] = {
-              id,
-              originalId: id.length > 36 ? id.slice(-36) : id,
-              score,
-              status,
-              startTime: +new Date(startTime),
-              home: {
-                name: home,
-                currentPoint: homeCurrentPoint,
-                side: sideHome
-              },
-              away: {
-                name: away,
-                currentPoint: awayCurrentPoint,
-                side: sideAway
-              },
-              markets,
-              mapIndex,
-              tournamentName,
-              tournamentId,
-              bo
-            }
-            resolve(result)
-          } catch (e) {
-            console.log(e)
-          }
-        }
+        handleMatched(matches, result)
       } else if (data && data.payload && data.payload.data && data.payload.data.sportEventListByFilters) {
-        const result = {}
         const matches = data.payload.data.sportEventListByFilters.sportEvents
-
-        for (const match of matches.filter(Boolean)) {
-          try {
-            const { id, fixture, markets, meta } = match
-            const { competitors, score, status, startTime, tournament } = fixture
-
-            const { value: mapIndex } = meta.find(spec => spec.name === "state_number") || {}
-            const { value: sideAway } = meta.find(spec => spec.name === "side_away") || {}
-            const { value: sideHome } = meta.find(spec => spec.name === "side_home") || {}
-            const { value: bo } = meta.find(spec => spec.name === "bo") || {}
-
-
-            const { name: tournamentName, id: tournamentId } = tournament
-            const { name: home, score: homeScore } = competitors.find(cmp => /home/i.test(cmp.homeAway))
-            const { name: away, score: awayScore } = competitors.find(cmp => /away/i.test(cmp.homeAway))
-
-            const { points: homeCurrentPoint } = homeScore.find(score => score.number === parseInt(mapIndex))
-            const { points: awayCurrentPoint } = awayScore.find(score => score.number === parseInt(mapIndex))
-
-            result[id] = {
-              id,
-              originalId: id.length > 36 ? id.slice(-36) : id,
-              score,
-              status,
-              startTime: +new Date(startTime),
-              home: {
-                name: home,
-                currentPoint: homeCurrentPoint,
-                side: sideHome
-              },
-              away: {
-                name: away,
-                currentPoint: awayCurrentPoint,
-                side: sideAway
-              },
-              mapIndex,
-              markets,
-              tournamentName,
-              tournamentId,
-              bo
-            }
-            resolve(result)
-          } catch (e) {
-            console.log(e)
-          }
-        }
+        handleMatched(matches, result)
       }
+      if (Object.keys(result).length)
+        resolve(result)
     } catch (e) {
       // console.log(e)
     }
@@ -113,9 +70,10 @@ async function getAllMatches(browserPage) {
   await f12.send('Network.enable')
   await f12.send('Page.enable')
 
-  await new Promise((resolve) => {
+  const result = await new Promise((resolve) => {
     f12.on('Network.webSocketFrameReceived', params => handleWebSocketFrameReceived(params, resolve))
   })
+  return result
 }
 
 async function getMatches(browserPage, matchListUpdateCb, matchUpdateCb) {
@@ -307,6 +265,10 @@ async function createBrowserAndPage(args) {
   const page = await browser.newPage()
   await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36')
   await page.setViewport({ width: 1920, height: 1080 })
+  const environment = process.env.NODE_ENV
+  if (environment != "production") {
+    page.on('console', msg => console.log('PAGE LOG:', msg.text()));
+  }
   return { browser, page }
 }
 
@@ -338,6 +300,20 @@ async function getLiveLine(discipline, matchListUpdateCb, matchUpdateCb, args, {
 
   await page.goto(url, { waitUntil: 'domcontentloaded' })
 
+  // 修改ws请求参数，让其返回完整的market数据
+  await page.evaluate(() => {
+    WebSocket.prototype.oldSend = WebSocket.prototype.send;
+
+    WebSocket.prototype.send = function (data) {
+
+      obj = JSON.parse(data)
+      if (obj.type == "start") {
+        obj.payload.variables.isTopMarkets = false
+      }
+      WebSocket.prototype.oldSend.apply(this, [JSON.stringify(obj)]);
+    };
+  })
+
   await page.waitForXPath("//span[contains(., 'Live')]/parent::div", { timeout: 0 })
   const [button] = await page.$x("//span[contains(., 'Live')]/parent::div");
 
@@ -352,20 +328,6 @@ async function getLiveLine(discipline, matchListUpdateCb, matchUpdateCb, args, {
   });
 
   console.log("start get live odds");
-
-  // 修改ws请求参数，让其返回完整的market数据
-  await page.evaluate(() => {
-    WebSocket.prototype.oldSend = WebSocket.prototype.send;
-
-    WebSocket.prototype.send = function (data) {
-
-      obj = JSON.parse(data)
-      if (obj.type == "start") {
-        obj.payload.variables.isTopMarkets = false
-      }
-      WebSocket.prototype.oldSend.apply(this, [JSON.stringify(obj)]);
-    };
-  })
 
 
   const matches = await getMatches(page, matchListUpdateCb, matchUpdateCb)
@@ -396,6 +358,23 @@ async function getAllLine(discipline, args, {
 
   await page.goto(url, { waitUntil: 'domcontentloaded' })
 
+  // 修改ws请求参数，让其返回完整的market数据
+  await page.evaluate(() => {
+    WebSocket.prototype.oldSend = WebSocket.prototype.send;
+
+    WebSocket.prototype.send = function (data) {
+
+      obj = JSON.parse(data)
+      if (obj.type == "start") {
+        obj.payload.variables.isTopMarkets = false
+        if (obj?.payload?.variables?.marketLimit) {
+          obj.payload.variables.marketLimit = 0
+        }
+      }
+      WebSocket.prototype.oldSend.apply(this, [JSON.stringify(obj)]);
+    };
+  })
+
   await page.waitForXPath("//span[contains(., 'Upcoming')]/parent::div", { timeout: 0 })
   const [button] = await page.$x("//span[contains(., 'Upcoming')]/parent::div");
 
@@ -410,21 +389,6 @@ async function getAllLine(discipline, args, {
   });
 
   console.log("start get all odds");
-
-  // 修改ws请求参数，让其返回完整的market数据
-  await page.evaluate(() => {
-    WebSocket.prototype.oldSend = WebSocket.prototype.send;
-
-    WebSocket.prototype.send = function (data) {
-
-      obj = JSON.parse(data)
-      if (obj.type == "start") {
-        obj.payload.variables.isTopMarkets = false
-      }
-      WebSocket.prototype.oldSend.apply(this, [JSON.stringify(obj)]);
-    };
-  })
-
 
   const matches = await getAllMatches(page)
   await page.close()
